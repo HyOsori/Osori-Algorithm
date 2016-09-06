b5672a9282735d1e2b4924f58f9232fd2532ca80
NaHut <whow1111@naver.com>
2016-09-07 01:03:49 +0900

    I will revise files

---
+++ HyeongJun/sort/src/counting_sort.h
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "swap.h"
+
+FILE* fi;
+
+int maximum(int arr[], int length);
+void counting_sort(int arr[], int tarr[], int length, int tlength);
+int maximum(int arr[], int length) {
+	int max = arr[0];
+	int i;
+	for (i = 0; i < length; i++) {
+		if (max < arr[i]) {
+			max = arr[i];
+		}
+	}
+	return max;
+}
+void counting_sort(int arr[], int tarr[], int length, int tlength) {
+	int i;
+	int* temp = (int*)malloc(sizeof(int)*length);
+	for (i = 0; i < tlength; i++) {
+		tarr[i] = 0;
+	}
+	for (i = 0; i < length; i++) {
+		tarr[arr[i]]++;
+	}
+	for (i = 1; i < tlength; i++) {
+		tarr[i] += tarr[i - 1];
+	}
+	for (i = length - 1; i >= 0; i--) {
+		temp[tarr[arr[i]]-1] = arr[i];
+		tarr[arr[i]]--;
+	}
+	for (i = 0; i < length; i++) {
+		arr[i] = temp[i];
+	}
+}
---
+++ HyeongJun/sort/src/quick_sort.h
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include "swap.h"
+
+//I select the pivot randomly, and then swap it and the start element of array
+//Is it okay?
+
+FILE* fi;
+void quick_sort(int arr[], int p, int r);
+int partition(int arr[], int p, int r);
+void print_arr(int arr[], int length);
+void quick_sort(int arr[], int p, int r) {
+	int q;
+	if (p < r) {
+		q = partition(arr, p, r);
+		quick_sort(arr, p, q - 1);
+		quick_sort(arr, q + 1, r);
+	}
+}
+int partition(int arr[], int p, int r) {
+	srand(time(NULL));
+	int pivot = -1;
+	int i = p;
+	int j = i + 1;
+	while (pivot < p) {
+		pivot = rand() % (r + 1);
+	}
+	
+	swap(&arr[p], &arr[pivot]);
+	pivot = arr[i];
+	while (j <= r) {
+		if(pivot>arr[j]){
+			swap(&arr[++i],&arr[j++]);
+		}
+		else{
+			j++;
+		}
+	}
+	swap(&arr[p], &arr[i]);
+	
+
+	return i;
+}
+void print_arr(int arr[], int length) {
+	int i;
+	for (i = 0; i < length; i++) {
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+}
