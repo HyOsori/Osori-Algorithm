4870d5dcf0ccbc3afcdb5a039fb86cde75434a20
johyung <epikjjh@gmail.com>
2016-09-15 18:01:02 +0900

    only sorting not yet skip list

--- johyeong/main.c
+++ johyeong/main.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void)
-{
-	printf("Hello World\n");
+typedef struct _skip_list_node{
+	struct **_skip_list;
+	int key;
+	void *val;
+	float p;
+}skip_list_node;
+
+typedef struct _skip_list{
+	skip_list_node header;
+	int level;
+	int max_level;
+}skip_list;
+
+void intit();
+
+int main(){
+			
+
+	return 0;
+}
+
+void init(){
+
 }
